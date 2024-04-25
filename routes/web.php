aa03e8c8e2b98e46b93896b457ea3c90061d86ee
João Ribeiro <jribeiro@gmail.com>
2024-04-25 17:58:18 +0100

    037

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,9 @@ Route::middleware([CheckIsLogged::class])->group(function(){
     Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');
     Route::post('/editNoteSubmit', [MainController::class, 'editNoteSubmit'])->name('editNoteSubmit');
     
-    // edit note
+    // delete note
     Route::get('/deleteNote/{id}', [MainController::class, 'deleteNote'])->name('delete');
+    Route::get('/deleteNoteConfirm/{id}', [MainController::class, 'deleteNoteConfirm'])->name('deleteConfirm');
 
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
