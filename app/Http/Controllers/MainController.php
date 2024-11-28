545fdc298a34343c03aed2e6596692424ee5d7c7
Diego Torino <diegotorinodev@gmail.com>
2024-11-28 15:50:51 -0300

    011

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,9 +8,12 @@ class MainController extends Controller
 {
     public function index()
     {
-        echo "I'm inside the app!";
+        // load user's notes
+
+        //show home view
+        return view('home');
     }
-}
+
     public function newNote()
     {
         echo "I'm creating a new note! ";
