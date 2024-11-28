04783d0dd77e7e3535bea731e07609a2ed234539
Diego Torino <diegotorinodev@gmail.com>
2024-11-28 15:28:39 -0300

    009

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,19 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIsLogged;
 use Illuminate\Support\Facades\Route;
 
 //auth routes
 Route::get('/login', [AuthController::class, 'login'] );
 route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
-Route::get('/logout', [AuthController::class, 'logout'] );
+
+route::middleware([CheckIsLogged::class])->group(function(){
+    
+    Route::get('/', [MainController::class, 'index'] );
+    Route::get('/newNote', [MainController::class, 'newNote'] );
+    Route::get('/logout', [AuthController::class, 'logout'] );
+});
+
+
+
